ba11b81233afe233437516d03c75fe8a432963c8
WebsiteCubed <=>
2023-08-09 19:02:20 +0100

    Add project files.

---
+++ TodoApi/Todo.cs
@@ -0,0 +1,9 @@
+﻿
+public class Todo
+{
+    public int Id { get; set; }
+    public String? Name { get; set; }
+    public bool IsComplete { get; set; }
+    public string? Secret { get; set; }
+}
+
